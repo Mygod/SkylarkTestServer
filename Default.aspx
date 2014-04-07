71bd02179b93be080432be36bbde471f0a2b90db
Mygod <mygodstudio@gmail.com>
2014-04-07 20:09:57 +0800

    Initial commit

---
+++ Default.aspx
@@ -0,0 +1,4 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs"
+         Inherits="Mygod.Skylark.TestServer.Default" %>
+<%=string.Format("{0} / {1} ({2:0.00} %)", GetSize(CurrentDrive.AvailableFreeSpace), GetSize(CurrentDrive.TotalSize),
+                                           100.0 * CurrentDrive.AvailableFreeSpace / CurrentDrive.TotalSize) %>
---
+++ Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Web.UI;
+
+namespace Mygod.Skylark.TestServer
+{
+    public partial class Default : Page
+    {
+        protected DriveInfo CurrentDrive;
+
+        private static readonly string[]
+            Units = { "字节", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB", "BB", "NB", "DB", "CB" };
+        public static string GetSize(long size)
+        {
+            double byt = size;
+            byte i = 0;
+            while (byt > 1000)
+            {
+                byt /= 1024;
+                i++;
+            }
+            return i == 0 ? size.ToString("N0") + " 字节"
+                          : byt.ToString("N") + " " + Units[i] + " (" + size.ToString("N0") + " 字节)";
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            CurrentDrive = new DriveInfo(Server.MapPath("/")[0].ToString(CultureInfo.InvariantCulture));
+        }
+    }
+}
